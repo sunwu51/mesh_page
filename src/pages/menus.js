96e7f22327166d6d2f48c827ad0f95311e5cb7fe
Frank <sunwu51@126.com>
2018-01-17 17:02:28 +0800

    clear pages

--- src/pages/menus.js
+++ src/pages/menus.js
@@ -1,7 +1,5 @@
 export const menus = [
     { key: '/app/dashboard/index', title: '首页', icon: 'mobile', },
-    { name:'user',key: '/app/user', title: 'user', icon: 'rocket', },
-	{ name:'org',key: '/app/org', title: 'org', icon: 'rocket', },
 	/*anchor*/  
     /*请不要删除上面这行注释，这是自动生成代码的一个钩子*/  
 ];
