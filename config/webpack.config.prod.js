29e09693c270a39792bd3e4a87ac4239402d4807
Frank <sunwu51@126.com>
2018-01-17 23:27:27 +0800

    fix babel-plugin-import for antd

--- config/webpack.config.prod.js
+++ config/webpack.config.prod.js
@@ -187,7 +187,7 @@ module.exports = {
         loader: require.resolve('babel-loader'),
         options: {
             plugins: [
-                ['import', [{ libraryName: 'antd', style: true }]],  // import less
+                ['import', [{ libraryName: 'antd', libraryDirectory: 'es', style: 'css' }]],  // import less
             ],
           compact: true,
         },
